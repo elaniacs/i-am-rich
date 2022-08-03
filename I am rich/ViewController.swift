862ea0d4373370988742dc680e22fd8a9b8df133
elaniacs <carensousaesilva@gmail.com>
2022-08-03 13:14:12 -0300

    add local project

---
+++ I am rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by Cáren Sousa on 03/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
